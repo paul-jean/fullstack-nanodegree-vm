7b37f2d6bad88b986bc4e411e1bb1b61fb884679
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-04-07 22:03:11 -0400

    Wiring up POST actions from restaurant create form.

--- vagrant/restaurant/webserver.py
+++ vagrant/restaurant/webserver.py
@@ -1,6 +1,8 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-from restaurant_queries import get_restaurants
+import restaurant_queries
+from restaurant_queries import get_restaurants, add_restaurant, \
+        restaurant_exists
 
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -28,13 +30,15 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+                print "restaurants/new"
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Add restaurant</h1>"
                 output += """
-                        <form action="cgi-bin/add_restaurant.cgi" method="post">
+                        <form action=""
+                        enctype="multipart/form-data" method="post">
                             <label for="rest_name">Name:</label>
-                            <input type="text" id="rest_name">
+                            <input type="text" name="rest_name">
                             <input type="submit" value="submit">
                         </form>
                         """
@@ -53,11 +57,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write("confirm-delete")
                 return
-
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
-'''
     def do_POST(self):
         try:
             self.send_response(301)
@@ -65,27 +67,45 @@ class webserverHandler(BaseHTTPRequestHandler):
             ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+            rest_name = fields.get('rest_name')[0]
+            print "rest_name:"
+            print rest_name
+            exists = restaurant_exists(rest_name)
+            print "exists:"
+            print exists
+            add_result = ""
+            if exists:
+                add_result += "<p>Restaurant %s already exists!</p>" % rest_name
+                print add_result
+            elif re.match("[A-Za-z0-9' ]+", rest_name) == None:
+                add_result += "<p>Restaurant name %s is not valid.</p>" % rest_name
+                add_result += "<p>Restaurant name can contain only alpha-numeric\
+                        characters: [A-Za-z0-9' ]+</p>"
+                print add_result
+            else:
+                add_result += "<p>Adding restaurant %s ...</p>" % rest_name
+                print add_result
+                add_restaurant(rest_name)
+            print "add_result:"
+            print add_result
             output = ""
             output += "<html><body>"
-            output += "<h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output +=\
-            """\
-            <form method='POST'
-                enctype='multipart/form-data'
-                action='/hello'>
-                <h2>What would you like me to say?</h2>
-                <input name="message" type="text" ><input type="submit" value="Submit">
-            </form>\
-            """
+            output += add_result
+            output += "<h1>Add restaurant</h1>"
+            output += """
+                    <form action="/restaurants/new"
+                    enctype="multipart/form-data" method="post">
+                        <label for="rest_name">Name:</label>
+                        <input type="text" id="rest_name">
+                        <input type="submit" value="submit">
+                    </form>
+                    """
             output += "</body></html>"
             self.wfile.write(output)
             print output
             return
         except:
             pass
-'''
 
 
 def main():
