385dc52edb42d23d4b335a089e2bbc3a80c632fa
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-04-19 22:56:02 -0400

    Add POST requests for adding menu items.

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,15 +14,21 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-@app.route('/restaurants/<int:restaurant_id>/')
-def menuItems(restaurant_id):
-    rest = session.query(Restaurant).filter_by(id = restaurant_id).one()
+@app.route('/restaurants/<int:rest_id>/')
+def menuItems(rest_id):
+    rest = session.query(Restaurant).filter_by(id = rest_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = rest.id).all()
     return render_template("menu.html", restaurant=rest, items=items)
 
-@app.route("/restaurant/<int:rest_id>/new/")
+@app.route("/restaurant/<int:rest_id>/new/", methods = ['GET', 'POST'])
 def newMenuItem(rest_id):
-    return "new menu item"
+    if request.method == "POST":
+        newItem = MenuItem(name = request.form["name"], restaurant_id = rest_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for("menuItems", rest_id = rest_id))
+    else:
+        return render_template("new-menu-item.html", restaurant_id = rest_id)
 
 @app.route("/restaurant/<int:rest_id>/<int:menu_id>/edit/")
 def editMenuItem(rest_id, menu_id):
